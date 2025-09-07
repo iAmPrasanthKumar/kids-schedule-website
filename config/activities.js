7038bdfef19eb80cc37f64405cbdfc79253dd998
Prasanth Kumar Allam <kumarprasanth119@gmail.com>
2025-09-07 08:40:47 +0530

    Color change

--- config/activities.js
+++ config/activities.js
@@ -96,7 +96,7 @@ const activitiesData = [
     "startTime": "20:00",
     "endTime": "20:30",
     "label": "Bedtime Stories",
-    "color": "purple",
+    "color": "violet",
     "description": "Calming story time to relax the mind and prepare for peaceful sleep",
     "aiSuggestions": bedtimeStories,
     "aiEnabled": true
